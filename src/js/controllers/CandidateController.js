9e45d724793bf92ca27192d6459c69f721051d67
Emiliano Ortiz <eortiz@lat.ar>
2018-10-05 14:13:49 -0300

    update

--- src/js/controllers/CandidateController.js
+++ src/js/controllers/CandidateController.js
@@ -4,10 +4,16 @@ angular.module('EulenApp')
 
 .controller('CandidateController', ['$scope', '$state',
     function ($scope,$state){
+        $scope.countries= [
+            "Alemania",
+            "España",
+            "Francia",
+            "Inglaterra",
+        ]
 
         $scope.candidate = {
             D_APELLIDO: null,
-            
+
         }
     }
 ])
